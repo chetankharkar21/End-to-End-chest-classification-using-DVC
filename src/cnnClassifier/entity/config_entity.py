59b701cb6ba6269bf4d211f62959359b2abc7fc4
chetankharkar21 <shivkharkar99@gmail.com>
2024-12-28 00:12:08 -0500

    model trainer added

--- src/cnnClassifier/entity/config_entity.py
+++ src/cnnClassifier/entity/config_entity.py
@@ -19,3 +19,15 @@ class PrepareBaseModelConfig:
     params_include_top: bool            # Whether to include the top layer in the model
     params_weights: str                  # Pre-trained weights
     params_classes: int   
+
+
+@dataclass(frozen=True)
+class TrainingConfig:
+    root_dir:Path
+    trained_model_path: Path
+    updated_base_model_path:Path
+    training_data:Path
+    params_epochs:int
+    params_batch_size:int
+    params_is_agumentation:bool
+    params_image_size:list
